62ecb4f8a618e5dced64494695fc0e0b45569136
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-08 20:01:30 -0700

    added firebase auth

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import {View, Text} from 'react-native'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import reducers from './reducers'
+import firebase from 'firebase'
 
 class App extends React.Component {
+    componentDidMount(){
+        const firebaseConfig = {
+            apiKey: "AIzaSyAGEK6v1NuzRdHP-t1dXphX7W6zcStoulo",
+            authDomain: "manager-7f78b.firebaseapp.com",
+            databaseURL: "https://manager-7f78b.firebaseio.com",
+            projectId: "manager-7f78b",
+            storageBucket: "",
+            messagingSenderId: "870492058080",
+            appId: "1:870492058080:web:79609a539af65e13"
+          };
+          firebase.initializeApp(firebaseConfig)
+    }
+
     render(){
         return (
             <Provider store = {createStore(reducers)}>
-            <View>
-                <Text>
-                    Hello!
-                </Text>
-            </View>
-        </Provider>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
         )
     }
 }
