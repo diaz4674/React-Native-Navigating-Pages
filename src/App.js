db138a445755b65aafa5a214695d974f6b151183
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-08 20:15:49 -0700

    login inputs work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import reducers from './reducers'
 import firebase from 'firebase'
+import LoginForm from './components/LoginForm';
 
 class App extends React.Component {
     componentDidMount(){
@@ -22,11 +23,7 @@ class App extends React.Component {
     render(){
         return (
             <Provider store = {createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+                 <LoginForm />
             </Provider>
         )
     }
